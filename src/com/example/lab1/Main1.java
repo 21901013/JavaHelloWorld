8d0f785a616ef42d2c4c8756cc93dd7c610673fb
Ye Eun <21901013@handong.edu>
2021-09-05 21:39:20 +0900

    2nd commit push

---
+++ src/com/example/lab1/Main1.java
@@ -0,0 +1,16 @@
+package com.example.lab1;
+
+import java.util.Scanner;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		System.out.println("Hello World!!!/n");
+		System.out.print("이름 입력 : ");
+		String name = s.next();
+		System.out.println("["+name+"]님 2021년에 가입을 환영합니다!");
+	}
+
+}
